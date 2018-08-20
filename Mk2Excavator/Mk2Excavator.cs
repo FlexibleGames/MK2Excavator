09bbd2297a27cd2eead75fa9e9b3de72498b315e
BuildTools <unconfigured@null.spigotmc.org>
2018-08-20 00:36:10 -0700

    cleanup imports

--- Mk2Excavator/Mk2Excavator.cs
+++ Mk2Excavator/Mk2Excavator.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class Mk2Excavator : MachineEntity, PowerConsumerInterface
